3495cf02e56ee41a8518d5a6984b2b96f07fdea7
Rauulito <7rnavarro7@gmail.com>
2023-01-07 13:11:34 +0100

    nuestra clase

--- hola_pyside.py
+++ hola_pyside.py
@@ -11,7 +11,7 @@ app.exec_()
 
 
 def hola(self):
- print("¡Hola mundo!")
+    print("¡Hola mundo!")
 # ...
 # Creamos el botón y lo conectamos
 button = QW.QPushButton("Hola", window)
@@ -26,7 +26,25 @@ button.clicked.connect(hola)
 # Mostramos la ventana principal
 window.show()
 
-
+#Nuestra propia clase
+class MainWindow(QW.QMainWindow):
+# Creamos nuestra ventana a partir de un QMainWindow
+    def _init_(self):
+        # Con super ejecutamos su propio constructor
+        # Así se crea la ventana en su propia instancia self
+        super()._init_()
+        # Redimensionamos y añadimos el botón
+        self.resize(200, 100)
+        button = QW.QPushButton("Hola", self)
+        button.clicked.connect(self.hola)
+        # Finalmente mostramos la ventana
+        self.show()
+        def hola(self):
+            print("¡Hola mundo!")
+# Creamos la aplicación, la ventana e iniciamos el bucle
+app = QW.QApplication()
+window = MainWindow()
+app.exec_()
 
 
 
